a248900f682b911058c27e2d4941497f8a27b572
felipe.yoshida <felipe.yoshida@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-09-25 19:28:51 +0000

    Bug com emulador/angel no handler

--- mutex/irq.c
+++ mutex/irq.c
@@ -5,23 +5,25 @@ extern int  Angel_IRQ_Address;
 // Installs a handler to the interruption vector
 void irq_installhandler (unsigned routine, unsigned *vector) { 
 	
-	/*unsigned vec;
-	// Checking if the function is in range
-	vec = ((routine - (unsigned)vector - 0x8)>>2);
-	vec = 0xea000000 | vec;
-	*vector = vec;
-	*/
+	if (emulator == 1) {
+		unsigned vec;
+		// Checking if the function is in range
+		vec = ((routine - (unsigned)vector - 0x8)>>2);
+		vec = 0xea000000 | vec;
+		*vector = vec;
+	}
+	else {
 	
-	unsigned old_vector_value = 0;
-	unsigned *segundo;
+		unsigned old_vector_value = 0;
+		unsigned *segundo;
   
-   	old_vector_value = 0;
-  	old_vector_value  = *vector;
-   	old_vector_value ^= 0xe59ff000;
+   		old_vector_value = 0;
+  		old_vector_value  = *vector;
+   		old_vector_value ^= 0xe59ff000;
 
-   	segundo = (unsigned *) (0x18 + old_vector_value+0x8); 
+   		segundo = (unsigned *) (0x18 + old_vector_value+0x8); 
 			
-   	Angel_IRQ_Address = *segundo;	  
-   	*segundo = routine;
-	
+   		Angel_IRQ_Address = *segundo;	  
+   		*segundo = routine;
+	}
 }
