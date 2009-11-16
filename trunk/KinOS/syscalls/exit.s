582c8cd8fe25aa01596516bf0e6bc69d3be51ba5
ramos.franco <ramos.franco@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-11-16 17:56:54 +0000

    Fork retornando id do filho pro pai e 0 para o filho.

--- trunk/KinOS/syscalls/exit.s
+++ trunk/KinOS/syscalls/exit.s
@@ -5,14 +5,14 @@
 	AREA	exit, CODE, READONLY
 	
 routine_exit
-	STMFD 	sp!,{r0-r12,lr}
+	STMFD 	sp!,{r0-r12,lr}			; save registers
 	MOV		r2, r1					; r2 = task id
-	LDR		r0, =thread_array
+	LDR		r0, =thread_array		; r0 = thread_array
 	MOV		r1, #0					; r1 is the state value = inactive
-	SUB		r2, r2, #1
-	MOV		r3, #4
-	MUL		r4, r2, r3				; r4 = (task number-1) * 4
-	ADD		r4, r0, r4				; r4 = r0 + r4
+	SUB		r2, r2, #1				; r2 = task id - 1
+	MOV		r3, #4					; r3 = 4
+	MUL		r4, r2, r3				; r4 = (task id - 1) * 4
+	ADD		r4, r0, r4				; r4 = thread_array + r4
 	STR		r1, [r4]				; Mem[r4] = r1 (inactive)
 	
 	LDMFD	sp!,{r0-r12,pc}^		; return
