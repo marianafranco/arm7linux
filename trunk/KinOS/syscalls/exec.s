395c62f2a2151cdb4eef57d978342dea459704ab
felipe.yoshida <felipe.yoshida@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-11-20 18:21:59 +0000

    Final comments

--- trunk/KinOS/syscalls/exec.s
+++ trunk/KinOS/syscalls/exec.s
@@ -1,55 +1,52 @@
+; Exec system call
+
 	EXPORT routine_exec
 	
 	IMPORT process_control_block
 	
-	AREA	exec, CODE, READONLY
+	; Beginning fork code
+	AREA	exec, CODE
 
-; Description: Sets up TASK Processor Control Block
-; taskPCB[-4]  = "LR" = &task
-; taskPCB[-8]  = "PCB SP" = SP_USER_BOTTOM - 4048;
-; taskPCB[-64] = "PC" = &task 
-; taskPCB[-68] = "SPSR"	= %nzcvift_User32 = 0x10
-
-
-; r2 = task address
-; r1 = task id 	
+; From the call of the function: r1 = task id, r2 = task address
 routine_exec
-	STMFD 	sp!,{r0-r12,lr}				; save registers
+; Store variables
+	STMFD 	sp!,{r0-r12,lr}		; Push r0-12 in the stack
 	
-	; -- Put the task address in task_pcb_address - 4
+; Put the task address in task_pcb_address - 4 (Process counter)
 	MOV		r0, r2						; r0 = task address
-	ADD		r0,r0,#4					; r0 = task address + 4
-	LDR		r3, =process_control_block	; r3 = the start address of the PCB area
-	MOV		r4,#68						; r4 = 68 (space for each process in the PCB)
-	MUL		r5,r1,r4					; r5 = (task id) * 68
-	ADD		r3,r3,r5					; r3 = PCB start address + r5
-	SUB		r3,r3,#4					; r3 = r3 - 4
-	STR		r0,[r3]						; MEM[r3] = r0
+	ADD		r0, r0, #4					; r0 = task address + 4 (+4 due to the pipeline)
+	LDR		r3, =process_control_block	; r3 = PCB bottom
+	MOV		r4, #68						; r4 = 68
+	MUL		r5, r1, r4					; r5 = (task id) * 68
+	ADD		r3, r3, r5					; r3 = PCB bottom + (task id) * 68
+	SUB		r3, r3, #4					; r3 = r3 - 4
+	STR		r0, [r3]					; MEM[r3] = r0
 
-	; -- set up USER stack for the TASK .................
-	SUB		r3,r3,#4					; r3 = r3 -4
-loop
-	SUB		r1,r1,#1					; r1 = task id - 1
-	CMP		r1,#0						; r1 = 0 ??
-	BEQ		end_loop					; if equal, end_loop
+; Set up user stack for the task
+	SUB		r3, r3, #4					; r3 = r3 - 4 (stack pointer)
 	MOV		r4, #0x20000				; r4 = SP_USER_BOTTOM
-	SUB		r0,r4,#4048					; r0 = r4 - 4048 (space for each process in the user stack)
-	B		loop						; return to loop
+loop
+	SUB		r1, r1, #1					; r1 = task id - 1
+	CMP		r1, #0						; r1 = 0 ?
+	BEQ		end_loop					; if equal, end_loop
+	SUB		r4, r4, #4048				; r0 = r4 - 4048 (next stack)
+	B		loop						; Go to next stack
 end_loop
-	STR		r0,[r3]						; MEM[r3] = r0 (the process stack pointer)
+	STR		r0,[r3]						; MEM[r3] = r4 (the process stack pointer)
 
-	; -- set up link register ............................
-	SUB		r3,r3,#56					; r3 = r3 - 56
-	MOV		r0,r2						; r0 = task address
-	ADD		r0,r0,#4					; r0 = r0 - 4
-	STR		r0,[r3]						; MEM[r3] = r0
+; Set up link register
+	SUB		r3, r3, #56					; r3 = r3 - 56 (link register)
+	MOV		r0, r2						; r0 = task address
+	ADD		r0, r0, #4					; r0 = r0 - 4
+	STR		r0, [r3]					; MEM[r3] = r0
 
-	; -- set up SPSR .....................................
-	; Obs: All process run in User mode
-	SUB		r3,r3,#4					; r3 = r3 - 4
-	MOV		r0,#0x10 					; r0 = 0x10 (user mode)
-	STR		r0,[r3]						; MEM[r3] = r0
+; Set up SPSR
+	SUB		r3, r3, #4					; r3 = r3 - 4
+	MOV		r0, #0x10 					; r0 = 0x10 (user mode)
+	STR		r0, [r3]					; MEM[r3] = r0
 	
-	LDMFD	sp!,{r0-r12,pc}^			; return
+; Return
+	LDMFD	sp!, {r0-r12,pc}^	; Pop r0-r12 and link register to process counter
 	
+	; End of assembly code
 	END
