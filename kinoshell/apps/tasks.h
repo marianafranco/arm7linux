bcf60e46cca133efb2a7447ea34f02d8d56ed377
ramos.franco <ramos.franco@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-12-12 18:40:48 +0000

    deletado o kinoshell

--- kinoshell/apps/tasks.h
+++
@@ -1,7 +0,0 @@
-#include "../peripherals/segment.h"
-#include "../interrupt/swi.h"
-
-
-void task1 (void);
-void task2 (int);
-void set_segment(int);
--- kinoshell/apps/terminal.h
+++
@@ -1,3 +0,0 @@
-int strcmp(char* str1, char* str2);
-
-void shell (void);
