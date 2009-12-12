a64a7cc156435fdee548d837d6e756199a85988d
ramos.franco <ramos.franco@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-12-12 13:06:43 +0000

    Comentários

--- KinOS/mutex/mutex.h
+++ KinOS/mutex/mutex.h
@@ -1,8 +1,42 @@
+/**************************************************************
+ * EXTERNAL
+ **************************************************************/
+ 
+extern unsigned volatile int semaphore; // do not access directly
+
+
+/**************************************************************
+ * MACROS
+ **************************************************************/
 
 #define WAIT 		while (semaphore==1) {} mutex_gatelock(); 
 #define SIGNAL 		mutex_gateunlock(); 	
 
-extern unsigned volatile int semaphore; // do not access directly
 
+/****************************************************************
+ * ROUTINES
+ ****************************************************************/
+
+
+/* -- mutex_gatelock ----------------------------------------------
+ *
+ * Description 	: Locks the semaphore... 
+ * 
+ * Parameters	: none...
+ * Return	: none...
+ * Notes	:
+ *
+ */
 void mutex_gatelock (void);
+
+
+/* -- mutex_gateunlock --------------------------------------------
+ *
+ * Description 	: Unlocks the semaphore ...
+ * 
+ * Parameters	: none...
+ * Return	: none...
+ * Notes	:
+ *
+ */
 void mutex_gateunlock (void);
