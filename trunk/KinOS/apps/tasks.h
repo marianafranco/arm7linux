d5f6a8931fab901a5c517e029fd18101dd8222c9
ramos.franco <ramos.franco@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-12-12 21:38:16 +0000

    Comentários no tasks.c

--- trunk/KinOS/apps/tasks.h
+++ trunk/KinOS/apps/tasks.h
@@ -1,13 +1,43 @@
-#include "../peripherals/segment.h"
-#include "../interrupt/swi.h"
-#include "../mutex/mutex.h"
-#include "../peripherals/led.h"
-#include "serial.h"
-#include "../interrupt/irq.h"
-#include "../peripherals/dips.h"
+/****************************************************************
+ * IMPORT
+ ****************************************************************/
 
+#include "segment.h"
+#include "swi.h"
+#include "mutex.h"
+#include "led.h"
+#include "serial.h"
+#include "irq.h"
+#include "dips.h"
 #include "tictactoe.h"
 
+
+/****************************************************************
+ * EXTERN
+ ****************************************************************/
+
+extern int current_thread_id;
+
+
+/****************************************************************
+ * DEFINES
+ ****************************************************************/
+
+struct name_address { 
+	char* name; 			// The task name
+	void (*task_ptr)(int); 	// Pointer to the task
+}; 
+
+
+/****************************************************************
+ * ROUTINES
+ ****************************************************************/
+
+int strcmper (char* str1, char* str2);
+pt2Task  get_task_addr(char* name);
+int get_state(int pid);
+int get_task_name_size(void);
+char* get_task_name(int index);
 
 void display_pid(int);
 void mutex_test (int);
@@ -18,19 +48,5 @@ void malicious_handler(int);
 void security_flaw (int);
 void dips_to_leds (int);
 void dips_to_segments (int);
-
 void play_tictactoe(int);
 
-extern int current_thread_id;
-
-int strcmper (char* str1, char* str2);
-pt2Task  get_task_addr(char* name);
-int get_state(int pid);
-int get_task_name_size(void);
-char* get_task_name(int index);
-
-
-struct name_address { 
-	char* name; 			// The task name
-	void (*task_ptr)(int); 	// Pointer to the task
-}; 
