c9a4e7e95219bb13da6f7df266c777dcedb7e058
ramos.franco <ramos.franco@b6c864a0-9344-11de-9b71-cb34e3bcb0e2>
2009-12-12 12:44:14 +0000

    Comentários

--- trunk/KinOS/syscalls/fork.s
+++ trunk/KinOS/syscalls/fork.s
@@ -1,3 +1,20 @@
+;************************************************************************************
+;	KinOS - Microkernel for ARM Evaluator 7-T
+;	Seniors project - Computer Engineering
+;	Escola Politecnica da USP, 2009
+;	
+;	Felipe Giunte Yoshida
+;	Mariana Ramos Franco
+;	Vinicius Tosta Ribeiro
+;
+;
+; 
+;	The program was based on the mutex program by ARM - Strategic Support Group,
+;	contained on the ARM Evaluator 7-T example CD, under the folder /Evaluator7-T/
+;	source/examples/mutex/ 
+;************************************************************************************
+
+
 ; Fork system call
 
 	IMPORT	thread_array
